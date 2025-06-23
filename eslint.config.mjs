8374c99801efa71b6b171983397a6a8d60161f11
Jinef <jinefjohn@gmail.com>
2025-06-23 18:10:19 +0300

    fix: ran migrations on eslinttr.json to .mjs to fix eslint errors

---
+++ eslint.config.mjs
@@ -0,0 +1,52 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores([
+    "**/node_modules/",
+    "**/dist/",
+    "**/build/",
+    "**/.next/",
+    "**/*.config.js",
+    "**/*.config.ts",
+]), {
+    extends: compat.extends("eslint:recommended", "@typescript-eslint", "prettier"),
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.node,
+        },
+
+        parser: tsParser,
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+
+    rules: {
+        "@typescript-eslint/no-unused-vars": ["error", {
+            argsIgnorePattern: "^_",
+        }],
+
+        "@typescript-eslint/no-explicit-any": "warn",
+        "prefer-const": "error",
+        "no-var": "error",
+    },
+}]);
