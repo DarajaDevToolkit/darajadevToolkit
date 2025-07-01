bd4f37774bc0ddf098205c12b05c88beaa50b27f
Shady <shadymutethia@gmail.com>
2025-07-02 02:00:49 +0300

    feat: Add Forgot password page

---
+++ dashboard/src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,10 @@
+import AuthLayout from '@/components/ui/AuthLayout';
+import ForgotPasswordForm from '@/components/ui/ForgotPasswordForm';
+
+export default function ForgotPasswordPage() {
+  return (
+    <AuthLayout>
+      <ForgotPasswordForm />
+    </AuthLayout>
+  );
+}
